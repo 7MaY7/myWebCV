c8a8f5c0c959d1bc0c7e5157f15fc0cc0204ed0e
An.Maksymiv <an.maksymiv@gmail.com>
2016-11-07 17:29:11 +0200

    Add files via upload

--- js/scrollFade.js
+++ js/scrollFade.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $(document).scroll(function() {
     var y = $(this).scrollTop();
     if (y > 100) {
-      $('.skill').fadeIn(1000, function () {
+      $('.skills_info').fadeIn(1000, function () {
       	$(".gradJs").animate({width: '84%'},1000, function () {
       		$(".gradHtml").animate({width: '80%'},1000, function () {
       			$(".gradCss").animate({width: '80%'},1000, function () {
@@ -15,12 +15,38 @@ $(document).ready(function(){
       			});
       		});
       	});
-      });
+      });      
     } else {
-      $('.skill').fadeOut(0);
+      $('.skills_info').fadeOut(0);
+      
     }
   });
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+      $('.experience_info').fadeIn(1000);     
+    } else {
+      $('.experience_info').fadeOut(0); 
+    }
+  });
+
+    $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 1600) {
+      $('.education_info').fadeIn(1000);     
+    } else {
+      $('.education_info').fadeOut(0); 
+    }
+  });
+
+      $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 2400) {
+      $('.hobbies_info').fadeIn(1000);     
+    } else {
+      $('.hobbies_info').fadeOut(0); 
+    }
+  });
+
 });
-
-
- 
